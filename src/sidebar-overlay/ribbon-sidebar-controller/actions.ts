0ed3da0cfc7db32eb4c8b369057826c83f5f7d98
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-12-23 16:34:29 +0530

    Re-enable opening/closing sidebar & removal of ribbon

--- src/sidebar-overlay/ribbon-sidebar-controller/actions.ts
+++ src/sidebar-overlay/ribbon-sidebar-controller/actions.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Thunk } from './types'
 import { actions as ribbonActions } from './ribbon'
 import { actions as sidebarActions } from '../sidebar-common'
@@ -5,4 +6,20 @@ import { actions as sidebarActions } from '../sidebar-common'
 export const initState: () => Thunk = () => dispatch => {
     dispatch(ribbonActions.setIsExpanded(false))
     dispatch(sidebarActions.setSidebarOpen(false))
+=======
+import { getSidebarState } from '../utils'
+import { getTooltipState } from '../../content-tooltip/utils'
+import { Thunk } from '../types'
+import { actions as ribbonActions } from '../ribbon'
+
+/**
+ * Hydrates the Redux store on initial startup.
+ */
+export const initState: () => Thunk = () => async dispatch => {
+    const isRibbonEnabled = await getSidebarState()
+    const isTooltipEnabled = await getTooltipState()
+
+    dispatch(ribbonActions.setRibbonEnabled(isRibbonEnabled))
+    dispatch(ribbonActions.setTooltipEnabled(isTooltipEnabled))
+>>>>>>> Re-enable opening/closing sidebar & removal of ribbon
 }
