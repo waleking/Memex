23a78aa58d8b0b32a654524bb45c4ee1c7ac6006
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2019-01-05 21:31:52 +0530

    Add edit, delete and crowdfunding functionality to annotation box

--- src/common-ui/crowdfunding/components/CFBox.tsx
+++ src/common-ui/crowdfunding/components/CFBox.tsx
@@ -8,6 +8,8 @@ const styles = require('./CFBox.css')
 
 export interface Props {
     onClose: MouseEventHandler
+    tabs?: Tabs.Static
+    learnMoreUrl?: string
 }
 
 class CrowdfundingBox extends PureComponent<Props> {
