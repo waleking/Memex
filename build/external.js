8539359b5e7b41b56c953b97bdf34bf1430a7d16
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-03-29 12:14:21 +0700

    Add `storex-pattern-modules` submodule

--- build/external.js
+++ build/external.js
@@ -1,5 +1,6 @@
 export const externalTsModules = [
     '@worldbrain/storex',
     '@worldbrain/storex-backend-dexie',
+    '@worldbrain/storex-pattern-modules',
     '@worldbrain/memex-stemmer',
 ]
