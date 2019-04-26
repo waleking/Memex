fa83cd65d9d36ad6f1b7b5e39e96017bf820379f
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2019-04-26 12:24:09 +0700

    Rewrite search events lookups in storex + plugins
    - plugins for the complex lookback stuff

--- src/search/storex-plugins.ts
+++ src/search/storex-plugins.ts
@@ -2,10 +2,12 @@ import { AnnotationsListPlugin } from 'src/search/background/annots-list'
 import { PageUrlMapperPlugin } from 'src/search/background/page-url-mapper'
 import { SuggestPlugin } from 'src/search/search/suggest'
 import { DexieUtilsPlugin } from 'src/search/plugins/dexie-utils'
+import { SearchLookbacksPlugin } from 'src/search/plugins/search-lookbacks'
 
 export const plugins = [
     new AnnotationsListPlugin(),
     new PageUrlMapperPlugin(),
     new SuggestPlugin(),
     new DexieUtilsPlugin(),
+    new SearchLookbacksPlugin(),
 ]
